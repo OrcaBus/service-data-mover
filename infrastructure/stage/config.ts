0649661112c7c811bbff244384836baca37e95db
mmalenic <mmalenic1@gmail.com>
2025-11-12 17:42:57 +1100

    build: use new 0.0.96 constants

--- infrastructure/stage/config.ts
+++ infrastructure/stage/config.ts
@@ -3,45 +3,47 @@ import { DataMigrateStackProps } from './deployment-stack';
 import { getDefaultApiGatewayConfiguration } from '@orcabus/platform-cdk-constructs/api-gateway';
 import { VPC_LOOKUP_PROPS } from '@orcabus/platform-cdk-constructs/shared-config/networking';
 import { DATA_MOVER_ROLE_NAME } from './constants';
+import {
+  ANALYSIS_ARCHIVE_BUCKET,
+  FASTQ_ARCHIVE_BUCKET,
+  ONCOANALYSER_BUCKET,
+} from '@orcabus/platform-cdk-constructs/shared-config/s3';
+import {
+  FILE_MANAGER_BUCKETS,
+  FILE_MANAGER_CACHE_BUCKETS,
+} from '@orcabus/platform-cdk-constructs/shared-config/file-manager';
 
 export const getDataMigrateStackProps = (stage: StageName): DataMigrateStackProps => {
-  // let readFromBuckets = [];
-  // let deleteFromBuckets = [];
-  // let writeToBuckets = [];
-  // switch (stage) {
-  //   case "BETA":
-  //     // For dev/staging we can write to and read from the same set of buckets.
-  //     readFromBuckets = [oncoanalyserBucket[stage], icav2PipelineCacheBucket[stage]];
-  //     deleteFromBuckets = [oncoanalyserBucket[stage], icav2PipelineCacheBucket[stage]];
-  //
-  //     // For dev additionally, write to the filemanager inventory bucket for testing.
-  //     writeToBuckets = [
-  //       oncoanalyserBucket[stage],
-  //       icav2PipelineCacheBucket[stage],
-  //       fileManagerInventoryBucket[stage],
-  //     ];
-  //     break;
-  //   case "GAMMA":
-  //     readFromBuckets = [oncoanalyserBucket[stage], icav2PipelineCacheBucket[stage]];
-  //     deleteFromBuckets = [oncoanalyserBucket[stage], icav2PipelineCacheBucket[stage]];
-  //
-  //     writeToBuckets = [oncoanalyserBucket[stage], icav2PipelineCacheBucket[stage]];
-  //     break;
-  //   case "PROD":
-  //     readFromBuckets = [oncoanalyserBucket[stage], icav2PipelineCacheBucket[stage]];
-  //     deleteFromBuckets = [oncoanalyserBucket[stage], icav2PipelineCacheBucket[stage]];
-  //
-  //     // For prod, we only allow writing to the archive buckets, nothing else.
-  //     writeToBuckets = [icav2ArchiveAnalysisBucket[stage], icav2ArchiveFastqBucket[stage]];
-  //     break;
-  // }
+  let readFromBuckets = [];
+  let deleteFromBuckets = [];
+  let writeToBuckets = [];
+  switch (stage) {
+    case 'BETA':
+      // For dev we can write to and read from all filemanager buckets.
+      readFromBuckets = [...FILE_MANAGER_BUCKETS.BETA, ...FILE_MANAGER_CACHE_BUCKETS.BETA];
+      deleteFromBuckets = [...FILE_MANAGER_BUCKETS.BETA, ...FILE_MANAGER_CACHE_BUCKETS.BETA];
+      writeToBuckets = [...FILE_MANAGER_BUCKETS.BETA, ...FILE_MANAGER_CACHE_BUCKETS.BETA];
+      break;
+    case 'GAMMA':
+      readFromBuckets = [ONCOANALYSER_BUCKET.GAMMA, ...FILE_MANAGER_CACHE_BUCKETS.GAMMA];
+      deleteFromBuckets = [ONCOANALYSER_BUCKET.GAMMA, ...FILE_MANAGER_CACHE_BUCKETS.GAMMA];
+      writeToBuckets = [ONCOANALYSER_BUCKET.GAMMA, ...FILE_MANAGER_CACHE_BUCKETS.GAMMA];
+      break;
+    case 'PROD':
+      readFromBuckets = [ONCOANALYSER_BUCKET.PROD, ...FILE_MANAGER_CACHE_BUCKETS.PROD];
+      deleteFromBuckets = [ONCOANALYSER_BUCKET.PROD, ...FILE_MANAGER_CACHE_BUCKETS.PROD];
+
+      // For prod, we only allow writing to the archive buckets, nothing else.
+      writeToBuckets = [ANALYSIS_ARCHIVE_BUCKET, FASTQ_ARCHIVE_BUCKET];
+      break;
+  }
 
   return {
     vpcProps: VPC_LOOKUP_PROPS,
     dataMoverRoleName: DATA_MOVER_ROLE_NAME,
-    deleteFromBuckets: [],
-    readFromBuckets: [],
-    writeToBuckets: [],
+    deleteFromBuckets,
+    readFromBuckets,
+    writeToBuckets,
     logRetention: getDefaultApiGatewayConfiguration(stage).apiGwLogsConfig.retention,
   };
 };
